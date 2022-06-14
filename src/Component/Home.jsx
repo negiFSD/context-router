4aa8a517a961134d5e4df2cf668be4b07a345aef
DESKTOP-EBI9G0E\sonia <sonia.veintech@finxera.com>
2022-06-14 22:03:16 +0530

    faker intigration done

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import { faker } from '@faker-js/faker';
 
 function    Home() {
+  const productArrays = [...faker.datatype.array(20)].map(()=>({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.imageUrl()
+  }))
+  console.log(productArrays);
   return (
     <div>Home</div>
   )

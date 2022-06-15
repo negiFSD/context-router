61a3ac793c2732197cc9957cf9ab65b2813516bd
negiFSD <sunilnegi217@rediffmail.com>
2022-06-15 18:37:33 +0530

    added count in cart

--- src/Component/Cart.jsx
+++ src/Component/Cart.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import SingleCompo from "./SingleCompo";
 
 function Cart({cart, setCart}) {
-  console.log(cart)
+  console.log(cart.length)
   return (
     <div className="cart">
       <h1>My Cart</h1>
-      <p>Total : 100</p>
+      <p>Total :{cart.length}</p>
       <div className="classProduct">
      
       </div>
       <div>
-      {cart.map((prod)=>(<SingleCompo prod={prod} cart={cart} setCart={setCart}/>))}
+      {cart.map((prod)=>(<SingleCompo prod={prod} key={prod.id} cart={cart}  setCart={setCart}/>))}
       </div>  
     </div>
   );
